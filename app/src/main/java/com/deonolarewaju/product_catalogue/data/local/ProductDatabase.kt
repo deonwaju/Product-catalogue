96946519a1ebc93a687d05da960f203a0583d61a
Deon Olarewaju <Deonolarewaju@gmail.com>
2023-12-11 00:37:27 +0100

    creating products entity and dao, setiing up database

--- app/src/main/java/com/deonolarewaju/product_catalogue/data/local/ProductDatabase.kt
+++ app/src/main/java/com/deonolarewaju/product_catalogue/data/local/ProductDatabase.kt
@@ -2,12 +2,12 @@ package com.deonolarewaju.product_catalogue.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverter
+import androidx.room.TypeConverters
 import com.deonolarewaju.product_catalogue.data.local.converter.RoomDbConverters
 import com.deonolarewaju.product_catalogue.data.local.entities.ProductEntity
 
 @Database(entities = [ProductEntity::class], version = 1)
-@TypeConverter(RoomDbConverters::class)
+@TypeConverters(RoomDbConverters::class)
 abstract class ProductDatabase: RoomDatabase() {
-    abstract val productDao: ProductDao
+    abstract val productDao: ProductsDao
 }
