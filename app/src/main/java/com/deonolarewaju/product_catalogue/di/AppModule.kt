028c78c54cc7f95dd9c1e7e71e9d02c3fd323e2b
Deon Olarewaju <Deonolarewaju@gmail.com>
2023-12-11 12:49:35 +0100

    working on di

--- app/src/main/java/com/deonolarewaju/product_catalogue/di/AppModule.kt
+++ app/src/main/java/com/deonolarewaju/product_catalogue/di/AppModule.kt
@@ -1,11 +1,18 @@
 package com.deonolarewaju.product_catalogue.di
 
+import android.app.Application
+import androidx.room.Room
+import com.deonolarewaju.product_catalogue.BuildConfig
+import com.deonolarewaju.product_catalogue.data.local.room.ProductDatabase
+import com.deonolarewaju.product_catalogue.data.local.room.converter.RoomDbConverters
 import com.deonolarewaju.product_catalogue.data.remote.ProductApi
+import com.deonolarewaju.product_catalogue.util.Constants.PRODUCT_DATABASE
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -16,6 +23,24 @@ object AppModule {
     @Singleton
     fun provideProductsApi(): ProductApi {
         return Retrofit.Builder()
-            .baseUrl(BuildConfig)
+            .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ProductApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideProductDatabase(
+        application: Application
+    ): ProductDatabase {
+        return Room.databaseBuilder(
+            context = application,
+            ProductDatabase::class.java,
+            PRODUCT_DATABASE
+        )
+            .addTypeConverter(RoomDbConverters())
+            .fallbackToDestructiveMigration()
+            .build()
     }
 }
