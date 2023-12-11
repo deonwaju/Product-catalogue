1a11bfa7bff6a81f76d16f42bb183d6e2887d50c
Deon Olarewaju <Deonolarewaju@gmail.com>
2023-12-11 01:34:29 +0100

    working on IProductRDS.kt and ProductRDSImpl.kt

--- app/src/main/java/com/deonolarewaju/product_catalogue/data/remote/datasources/impl/ProductRDSImpl.kt
+++ app/src/main/java/com/deonolarewaju/product_catalogue/data/remote/datasources/impl/ProductRDSImpl.kt
@@ -1,6 +1,15 @@
 package com.deonolarewaju.product_catalogue.data.remote.datasources.impl
 
+import com.deonolarewaju.product_catalogue.data.remote.ProductApi
 import com.deonolarewaju.product_catalogue.data.remote.datasources.interfaces.IProductRDS
+import com.deonolarewaju.product_catalogue.domain.model.ProductsList
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
-class ProductRDSImpl: IProductRDS {
+class ProductRDSImpl(
+    private val api: ProductApi
+): IProductRDS {
+    override suspend fun fetchProducts(): Flow<ProductsList> = flow {
+        api.fetchAllProducts()
+    }
 }
